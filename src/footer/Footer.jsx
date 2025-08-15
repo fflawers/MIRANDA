43f2b988cc27ec2a5cd9fc0e2b030337f984029b
fflawers <fflaguers@gmail.com>
2025-08-14 20:26:50 -0600

    Added legals and some styles

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
+      <p><strong>Términos y Condiciones: <br/></strong> Al utilizar este sitio, el usuario acepta los términos relacionados con la compra, el uso de los productos, la propiedad intelectual y el cumplimiento de la legislación local. Las condiciones pueden cambiar sin previo aviso.</p>
+      <p><strong>Aviso de Privacidad: <br/></strong> La información personal del usuario será utilizada exclusivamente para fines de procesamiento de pedidos, envío de información promocional (previo consentimiento) y mejora de la experiencia. Se garantiza su protección conforme a la ley de protección de datos vigente según el país.</p>
+      <p><strong>Política de Devoluciones: <br/></strong> Aceptamos devoluciones dentro de los primeros 15 días después de la entrega. El producto debe estar sin uso y en su empaque original. El cliente debe contactar a nuestro servicio de atención para iniciar el proceso. No se aceptan devoluciones en productos de personalizados.</p>
       <p>&copy; 2025 Made by FFG. Todos los derechos reservados.</p>
     </footer>
   );
--- src/views/index.jsx
+++ src/views/index.jsx
@@ -7,7 +7,7 @@ const Index = () => {
     return (
         <div className="container">
             <div className="container-fluid">
-                <h1 className="text-center">Renueva tu ser, vive la elegancia del descanso</h1>
+                <h1 className="text-center titleIND">Renueva tu ser, vive la elegancia del descanso</h1>
                 <div className="">
                     <img className="d-flex mx-auto img-fluid img-spa1"
                         src="https://plus.unsplash.com/premium_photo-1679430672295-3846f0cf0503?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
